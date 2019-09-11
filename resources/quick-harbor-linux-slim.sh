ac655911e308f0b58b78099bc0c20449df144fe4
Alex Snow <asnow@ignw.io>
2019-09-11 11:37:44 -0700

    Preparing for a Jenkins test

--- resources/quick-harbor-linux-slim.sh
+++ resources/quick-harbor-linux-slim.sh
@@ -6,14 +6,17 @@ ip=$(terraform output | tr -d "instance-ip = -")
 # get the cert from the instance
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -v alexsnow@$ip:~/ca.crt .
 # Refresh the list of certificates to trust
-mkdir /etc/docker/certs.d/$ip
-mv ca.crt /etc/docker/certs.d/$ip/
+mkdir /etc/docker/certs.d/core.harbor.domain
+mv ca.crt /etc/docker/certs.d/core.harbor.domain/
+
+echo "'${ip}' core.harbor.domain" >> /etc/hosts
+
 # Restart the Docker daemon
 service docker restart
 
 sleep 60
 
 # login to docker and push a test image
-docker login --username admin --password Harbor12345 $ip
-docker tag hello-world:latest $ip/library/hello-world:latest
-docker push $ip/library/hello-world:latest
+docker login --username admin --password Harbor12345 core.harbor.domain
+docker tag hello-world:latest core.harbor.domain/library/hello-world:latest
+docker push core.harbor.domain/library/hello-world:latest
