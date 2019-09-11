d5214f7e3a0650ad0d1cead306287216f01e5164
Alex Snow <asnow@ignw.io>
2019-09-11 10:49:26 -0700

    Preparing for a Jenkins test

--- resources/quick-harbor-linux-slim.sh
+++ resources/quick-harbor-linux-slim.sh
@@ -4,9 +4,9 @@ yes yes | terraform apply -var-file=states/harbor-master-jenkins.tfvars
 ip=$(terraform output | tr -d "instance-ip = -")
 
 # get the cert from the instance
-scp -i /Users/alexsnow/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -v alexsnow@$ip:~/ca.crt .
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -v alexsnow@$ip:~/ca.crt .
 # Refresh the list of certificates to trust
-update-ca-certificates
+mv ca.crt /etc/docker/certs.d/$ip/
 # Restart the Docker daemon
 service docker restart
 
