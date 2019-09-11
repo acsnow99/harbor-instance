5b0fa0b4370b676facebdc330282cad20ddfad43
Alex Snow <asnow@ignw.io>
2019-09-11 10:59:09 -0700

    Preparing for a Jenkins test

--- resources/quick-harbor-linux-slim.sh
+++ resources/quick-harbor-linux-slim.sh
@@ -6,6 +6,7 @@ ip=$(terraform output | tr -d "instance-ip = -")
 # get the cert from the instance
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -v alexsnow@$ip:~/ca.crt .
 # Refresh the list of certificates to trust
+mkdir /etc/docker/certs.d/$ip
 mv ca.crt /etc/docker/certs.d/$ip/
 # Restart the Docker daemon
 service docker restart
